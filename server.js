6944594a55d38d627e1d535a97fa63573ebadc21
lucasteng123 <lucas.teng@ryerson.ca>
2018-05-22 10:19:24 +0100

    Add photosphere thumbnail creation

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var util = require('util');
 
 var gamingContent = false;
 
+var Content = require ('./Content.js');
+
 
 
 //image manipulation
@@ -35,67 +37,7 @@ create thumbnails from images
 UI
 */
 
-
-
-class Content {
-	constructor(contentFolder, imagesFolder, contentMode, inputContent, inputImgContent, imgContentFolder) {
-		// this._contentFolder = "./content/mediaFiles";
-		// this._imagesFolder = "./content/images";
-		this._contentFolder = contentFolder;
-		this._imagesFolder = imagesFolder;
-		this._content=[];
-		this._stillContent=[];
-		console.log("Loading Files");
-		//if it is set to load the entire folder
-		if (contentMode == 1) {
-			fs.readdirSync(this._contentFolder).forEach(videoFile => {
-				this.createThumbnail(videoFile);
-			});
-			//look through the settings json for content
-		} else {
-			for(var i = 0, max = inputContent.length; i<max; i++){
-				if (!fs.existsSync(this._imagesFolder + "/" + inputContent[i]["thumbnail"])) {
-					//console.log(vcRecord);
-					this.createThumbnail(inputContent[i]["videoName"],inputContent[i]["thumbnail"]);
-				}
-			}
-			this._content = inputContent;
-		}
-	}
-	createThumbnail(input,imgname) {
-		console.log("New Content");
-		var imgfld = this._imagesFolder;
-		var newProcessFile = new ffmpeg(this._contentFolder + "/" + input);
-		newProcessFile.then(function (video) {
-			video.fnExtractFrameToJPG(imgfld + "/temp", {
-				number: 1,
-				size: '1000x?',
-				every_n_percentage: 10,
-			}, function (error, files) {
-				if (!error){
-					console.log('ffmpeg: ' + files);
-					files.forEach(ffile=>{
-						var imgfilename = path.basename(ffile);
-						jimp.read(imgfld + "/temp/" + imgfilename, function (err, img) {
-							if (err) throw err;
-							img.cover(200, 200, jimp.HORIZONTAL_ALIGN_CENTER | jimp.VERTICAL_ALIGN_MIDDLE)
-								.quality(100)
-								.write(imgfld + "/" + imgname);
-							console.log("Cropped: " + imgfld + "/" + imgfilename);
-						});
-					});
-				}
-			});
-		}, function (err) {
-			console.log('Error: ' + err);
-		});
-	}
-
-	createThumbnailFromImage(input,output){
-		
-	}
-}
-var content = new Content(settings.mediaFiles, settings.imagesFolder, settings.contentMode, settings.videoContent);
+var content = new Content(settings.mediaFiles, settings.imagesFolder, settings.contentMode, settings.videoContent, settings.stillContent);
 
 //testing of OSC
 app.use('/oscTest', function (req, res, next) {
